bf78c82dd2badef120f9bb1981d2fcb320e814d7
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-05-11 13:56:36 +0300

    copy

---
+++ NCLEX/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 252, green: 250, blue: 245)
+    
+    static let mainColor = UIColor(integralRed: 244, green: 193, blue: 92)
+    static let mainColorAlpha = UIColor(integralRed: 244, green: 193, blue: 92, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 123, green: 131, blue: 246)
+    
+    static let progress1Color = UIColor(integralRed: 244, green: 193, blue: 92)
+    static let progress2Color = UIColor(integralRed: 123, green: 131, blue: 246)
+    static let progress3Color = UIColor(integralRed: 224, green: 113, blue: 131)
+}
